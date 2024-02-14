ad91f9d7e7be4dae2f62ae96ae78cfe183ee0068
Caspar Ritchie <caspar.ritchie@lapiscine.pro>
2024-02-14 13:33:18 +0100

    updating style-guide

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ def index():
     # Serve the index.html file
     return render_template('index.html')
 
+@app.route('/style-guide')
+def style_guide():
+    # Serve the style-guide.html file
+    return render_template('style-guide.html')
+
 if __name__ == "__main__":
     # Run the application
     app.run(debug=True)
